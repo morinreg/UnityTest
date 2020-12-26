c0009fdff70348344642718562bc4094b832ccdb
morinreg <76623338+morinreg@users.noreply.github.com>
2020-12-26 12:59:41 -0500

    Initial upload

---
+++ MyFirstGame - Edit/Assets/Scripts/ToggleDoor.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleDoor : MonoBehaviour
+{
+    public void ToggleVisibility()
+    {
+        gameObject.SetActive(false);
+    }
+}
